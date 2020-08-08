dbbd64c7dd92a5827a4874766ed2692207a71e73
kutycoi123 <tln3@sfu.ca>
2020-08-07 17:42:54 -0700

    Added lzw and animation

--- index.js
+++ index.js
@@ -17,24 +17,49 @@ function generate_string(len) {
 
 $(document).ready(function(){
 	function lzw(input) {
+		let result = {
+			encoded: "",
+			steps: []
+		};
 		let dict = {...INITIAL_DICT};
-		let i = 1, len = input.length, dict_len = Object.keys(dict).length;
-		let s = input[0];
+		let len = input.length, dict_len = Object.keys(dict).length;
+		let s = input[0];	
 		let encoded = [];
-		while (i < len) {
+		let s_start_idx = 0;
+		let s_end_idx = 0;
+		result.steps.push({
+			s: {start:0, end:0}
+		})
+		for(let i = 1; i < len; ++i){
 			let c = input[i];
+			result.steps.push({
+				c: i
+			})
 			if (dict[s+c]) {
 				s = s + c;
+				result.steps.push({
+					s: {start:s_start_idx, end:i},
+				});
+				s_end_idx = i;
 			}else {
 				encoded.push(dict[s]);
 				dict[s+c] = dict_len.toString();
 				dict_len++;
+				result.steps.push({
+					encoded: [...encoded],
+					old_s: {start: s_start_idx, end: s_end_idx},
+					s: {start: i, end: i},
+				})
+				s_start_idx = s_end_idx = i;
 				s = c;
 			}
-			i++;
 		}
 		encoded.push(dict[s]);
-		return encoded;
+		result["encoded"] = encoded;
+		result.steps.push({
+			old_s: {start:s_start_idx, end:s_end_idx}
+		});
+		return result;
 	}
 	var inputs = "";
 	inputs = "CCBDCCEAAAEACBBA"//generate_string(10);
@@ -45,7 +70,32 @@ $(document).ready(function(){
 	}
 	var handler = $("#handler");
 	handler.on("click", function(){
-		let encoded = lzw(inputs);
-		console.log(encoded);
+		let result = lzw(inputs);
+		console.log(result)
+		for (let i = 0; i < result.steps.length; ++i) {
+			let timeout = setTimeout(function(){
+				let step = result.steps[i];
+				if (step["old_s"]) {
+					for (let idx = step.old_s.start; idx <= step.old_s.end; ++idx){
+						$(`#sym-${idx}`).css({
+							backgroundColor:'white'
+						});
+					}
+				} 
+				if (step["s"]) {
+					for (let idx = step.s.start; idx <= step.s.end; ++idx) {
+						$(`#sym-${idx}`).css({
+							backgroundColor: 'red'
+						});
+					}
+				}
+				if (step["c"]) {
+					$(`#sym-${step.c}`).css({
+						backgroundColor: 'green'
+					});
+				}
+				clearTimeout(timeout);
+			},500*i);
+		}
 	})
 });
